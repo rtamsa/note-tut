f0badb02e530bbd2862edd4d76b8fba5f90c0834
Razvan Tamsa <razvan.tamsa@opswat.com>
2021-11-22 14:31:16 +0200

    no sense

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 var app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect( process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.MONGO_URI);
 
 app.use(function(req, res, next) {
   console.log(req.method + ' ' + req.path + ' - ' + req.ip);
