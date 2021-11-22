fed25e2fda8cbe8c2ba6d4aa60ffb7a4fccbf894
Razvan Tamsa <razvan.tamsa@opswat.com>
2021-11-22 14:14:57 +0200

    error in the logic

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose.connect( process.env['MONGO_URI'] , { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env['MONGO_URI'] || 'undefined' , { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(function(req, res, next) {
   console.log(req.method + ' ' + req.path + ' - ' + req.ip);
